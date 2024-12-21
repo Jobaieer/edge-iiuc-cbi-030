bed39bdfe54a385da3c40eadc593143d9503a30b
Jobaieer <jobaieer.cse@proton.me>
2024-12-21 22:35:15 +0600

    [Book-Shelf-React] Setup for project

---
+++ edge-iiuc-final-exam/book-shelf-react/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="flex flex-row justify-center items-center h-screen text-lg font-bold  hover:text-[#61DBFB] hover:text-[5rem]">
+        Jobaieer-React Project
+      </h1>
+    </>
+  );
+}
+
+export default App;
