0e3bc5b614c586f3572ab61db2cf86e98fe8ff9a
Jobaieer <jobaieer.cse@proton.me>
2024-12-26 15:37:39 +0600

    added Dashboard.jsx

--- edge-iiuc-final-exam/book-shelf-react/src/components/Dashboard.jsx
+++ edge-iiuc-final-exam/book-shelf-react/src/components/Dashboard.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
 
-export const Dashboard = () => {
-  return <div>Dashboard</div>;
+const Dashboard = ({ children }) => {
+  return (
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-row">
+
+        <Sidebar />
+
+        <div className="w-[100%]">
+          <Navbar />
+          <main className="flex-1 bg-gray-100 p-6 h-full">
+            {children}
+          </main>
+        </div>
+      </div>
+    </div>
+  );
 };
+
+export default Dashboard;
