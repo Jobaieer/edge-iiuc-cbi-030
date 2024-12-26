2d629322686fd0e194ea2e2ea51edc2ed48891d9
Jobaieer <jobaieer.cse@proton.me>
2024-12-26 15:33:23 +0600

    tailwind.config.js file gradient color added

--- edge-iiuc-final-exam/book-shelf-react/tailwind.config.js
+++ edge-iiuc-final-exam/book-shelf-react/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "gradient-63deg": "linear-gradient(-63deg, #571FCF, #EB5231)",
+      },
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
