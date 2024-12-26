bab9e869829a37ed64f445d68bf8b53ea27cc47f
Jobaieer <jobaieer.cse@proton.me>
2024-12-26 15:37:14 +0600

    added routes in App.jsx file

--- edge-iiuc-final-exam/book-shelf-react/src/App.jsx
+++ edge-iiuc-final-exam/book-shelf-react/src/App.jsx
@@ -1,14 +1,24 @@
-import "./App.css";
-import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Favorites from "./pages/Favorites";
+import MyShelf from "./pages/MyShelf";
 
-function App() {
+const App = () => {
   return (
-    <div className="flex">
-      <Sidebar />
-      <Navbar />
-    </div>
+    <Router>
+      <Dashboard>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Search" element={<Search />} />
+          <Route path="/MyShelf" element={<MyShelf/>} />
+          <Route path="/Favorites" element={<Favorites/>} />
+        </Routes>
+      </Dashboard>
+    </Router>
   );
-}
+};
 
 export default App;
